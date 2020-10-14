50dd462afa0eb9c686f0f9d5746a925b0f3759a7
wds1204 <wdsmyhome@hotmail.com>
2020-10-14 11:12:37 +0800

    玩转字体变色的自定义veiw----设置文字变色变化的方向

--- app/src/main/java/com/wds/viewkit/MainActivity.java
+++ app/src/main/java/com/wds/viewkit/MainActivity.java
@@ -25,18 +25,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void changeTextColor() {
         colorTrackTextView = findViewById(R.id.colorTrackTextView);
-
+        colorTrackTextView.setmDirection(ColorTrackTextView.Direction.DIRECTION_RIGHT);//设置从右到左
         ValueAnimator valueAnimator = ObjectAnimator.ofFloat(0, 1);
         valueAnimator.setDuration(3000);
         valueAnimator.setInterpolator(new DecelerateInterpolator());
+//        valueAnimator.setRepeatMode(ValueAnimator.RESTART);
+//        valueAnimator.setRepeatCount(2);
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 float currentStep = (float) animation.getAnimatedValue();
-                Log.e("TAG", "currentStep====="+currentStep);
                 colorTrackTextView.setmCurrentProgress(currentStep);
             }
         });
+        valueAnimator.setStartDelay(500);
         valueAnimator.start();
 
 
--- app/src/main/java/com/wds/viewkit/widget/ColorTrackTextView.java
+++ app/src/main/java/com/wds/viewkit/widget/ColorTrackTextView.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
+import android.icu.text.RelativeDateTimeFormatter;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -20,15 +23,21 @@ import com.wds.viewkit.R;
  * Description:
  */
 public class ColorTrackTextView extends androidx.appcompat.widget.AppCompatTextView {
-    int mStartColor,mEndColor;
-    Paint defaultPaint,changePaint;
-    float mCurrentProgress=0f;
+    int mStartColor, mEndColor;
+    Paint defaultPaint, changePaint;
+    float mCurrentProgress = 0f;
+    // 当前朝向
+    private Direction mDirection = Direction.DIRECTION_RIGHT;
+    // 绘制的朝向枚举
+    public enum Direction {
+        DIRECTION_LEFT, DIRECTION_RIGHT
+    }
     public ColorTrackTextView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public ColorTrackTextView(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public ColorTrackTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -41,13 +50,14 @@ public class ColorTrackTextView extends androidx.appcompat.widget.AppCompatTextV
 
 
         defaultPaint = new Paint();
-
+        // 仿抖动
+        defaultPaint.setDither(true);
         defaultPaint.setAntiAlias(true);
         defaultPaint.setColor(mStartColor);
         defaultPaint.setTextSize(getTextSize());
 
         changePaint = new Paint();
-
+        changePaint.setDither(true);
         changePaint.setAntiAlias(true);
         changePaint.setColor(mEndColor);
         changePaint.setTextSize(getTextSize());
@@ -57,41 +67,58 @@ public class ColorTrackTextView extends androidx.appcompat.widget.AppCompatTextV
 
     /**
      * 利用clipRect裁剪API 利用两个画笔不断的画
-     *
+     * <p>
      * 0～midValue，midValue～endvalue
+     *
      * @param canvas
      */
     @Override
     protected void onDraw(Canvas canvas) {
-//        super.onDraw(canvas);
-//        canvas.clipRect()  裁剪区域
-
-        canvas.save();
-
         /*绘制不变色的  0～midValu*/
-        int midValue= (int) (getWidth()*mCurrentProgress);
-        canvas.clipRect(0,0,midValue,getHeight());
+        int midValue = (int) (getWidth() * mCurrentProgress);
+        /*绘制  0～midValue的颜色*/
+        if( mDirection==Direction.DIRECTION_LEFT) {
+            drawText(canvas, 0, midValue, defaultPaint);
+            /*绘制 midValue～endvalue的颜色*/
+            drawText(canvas, midValue, getWidth(), changePaint);
+        }else {
+            drawText(canvas, 0,getWidth()- midValue, defaultPaint);
+            /*绘制 midValue～endvalue的颜色*/
+            drawText(canvas, getWidth()- midValue, getWidth(), changePaint);
+        }
 
-        String text = getText().toString();
-        Rect textBounds = new Rect();
-        defaultPaint.getTextBounds(text,0,text.length(),textBounds);
-        int dx=getWidth()/2-textBounds.width()/2;
-        Paint.FontMetrics fontMetrics = defaultPaint.getFontMetrics();
-        int baseLine= (int) ((fontMetrics.bottom-fontMetrics.top)/2-fontMetrics.bottom);;
-        int dy=getHeight()/2+baseLine;
-        canvas.drawText(text,dx,dy,defaultPaint);
+    }
 
-        canvas.restore();
-
-        /*绘制变色 midValue～endvalue*/
-        canvas.save();
-        canvas.clipRect(midValue,0,getWidth(),getHeight());
-        canvas.drawText(text,dx,dy,changePaint);
-        canvas.restore();
+    /**
+     * 设置方向
+     * @param mDirection
+     */
+    public void setmDirection(Direction mDirection) {
+        this.mDirection = mDirection;
     }
 
     public void setmCurrentProgress(float mCurrentProgress) {
         this.mCurrentProgress = mCurrentProgress;
         invalidate();
     }
+
+    public void drawText(Canvas canvas, int startValue, int endValue, Paint paint) {
+        canvas.save();
+
+        /*绘制不变色的  0～midValu*/
+        int midValue = (int) (getWidth() * mCurrentProgress);
+        canvas.clipRect(startValue, 0, endValue, getHeight());
+
+        String text = getText().toString();
+        Rect textBounds = new Rect();
+        defaultPaint.getTextBounds(text, 0, text.length(), textBounds);
+        int dx = getWidth() / 2 - textBounds.width() / 2;
+        Paint.FontMetrics fontMetrics = defaultPaint.getFontMetrics();
+        int baseLine = (int) ((fontMetrics.bottom - fontMetrics.top) / 2 - fontMetrics.bottom);
+
+        int dy = getHeight() / 2 + baseLine;
+        canvas.drawText(text, dx, dy, paint);
+        canvas.restore();
+    }
+
 }
